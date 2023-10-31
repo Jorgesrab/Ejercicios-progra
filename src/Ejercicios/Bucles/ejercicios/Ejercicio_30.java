41f407f451e0310451c19d2cc013056d449dd635
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2023-10-31 12:40:06 +0100

    commit

---
+++ src/Ejercicios/Bucles/ejercicios/Ejercicio_30.java
@@ -0,0 +1,23 @@
+package Ejercicios.Bucles.ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio_30 {
+    public static void main(String[] args) {
+       int limiteMax;
+       int contador=0;
+        Scanner entrada = new Scanner(System.in);
+
+
+       System.out.println("Introduce un numero superior a 1 ");
+        limiteMax = entrada.nextInt();
+
+
+
+
+
+
+
+
+    }
+}
