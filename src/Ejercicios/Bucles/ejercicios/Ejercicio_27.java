2a5c4c0fd374677876c542a73eda85304818351b
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2023-10-31 10:37:37 +0100

    commit

---
+++ src/Ejercicios/Bucles/ejercicios/Ejercicio_27.java
@@ -0,0 +1,29 @@
+package Ejercicios.Bucles.ejercicios;
+
+public class Ejercicio_27 {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        int horas=0;
+        int minutos=0;
+        int segundos=0;
+
+        while (horas<23 || minutos <59 || segundos<59){
+
+            segundos++;
+            if (segundos==60){
+                segundos=segundos-60;
+                minutos++;
+            }
+            if (minutos==60){
+                minutos=minutos-60;
+                horas++;
+            }
+            Thread.sleep(1000);
+            System.out.println(horas+":"+minutos+":"+segundos);
+
+
+
+        }
+    }
+}
