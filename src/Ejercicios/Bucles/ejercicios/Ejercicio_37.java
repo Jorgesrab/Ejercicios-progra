ddb2469a980bed29ab5f63a3fa4cc5c68745352f
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2023-11-06 11:52:32 +0100

    commit

---
+++ src/Ejercicios/Bucles/ejercicios/Ejercicio_37.java
@@ -0,0 +1,31 @@
+package Ejercicios.Bucles.ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio_37 {
+    public static void main(String[] args) {
+
+        int numeroMayor;
+        int numeroMenor;
+        int cantidadNumeros;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduce la cantidad de numeros que quieres");
+        cantidadNumeros = entrada.nextInt();
+        int temp;
+        for (int i = 0; i < cantidadNumeros; i++) {
+
+            System.out.println("Introduce un numero");
+            temp = entrada.nextInt();
+            if (temp = null){
+            if (temp>)
+
+
+
+
+        }
+
+
+    }
+}
