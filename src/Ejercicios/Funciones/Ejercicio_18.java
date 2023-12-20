aa4c184ec8c77efad80793e10a0b2abdc7ddac06
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2023-12-20 12:17:19 +0100

    funciones

---
+++ src/Ejercicios/Funciones/Ejercicio_18.java
@@ -0,0 +1,7 @@
+package Ejercicios.Funciones;
+
+public class Ejercicio_18 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Ejercicios/Funciones/Ejercicio_19.java
@@ -0,0 +1,7 @@
+package Ejercicios.Funciones;
+
+public class Ejercicio_19 {
+    public static void main(String[] args) {
+
+    }
+}
