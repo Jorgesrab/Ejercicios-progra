3d71d75ec454775b641d89e2ceaa3f09e45574bb
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2024-01-25 11:40:43 +0100

    array

--- src/Ejercicios/Arrays/Ejercicio_13.java
+++ src/Ejercicios/Arrays/Ejercicio_13.java
@@ -14,25 +14,29 @@ public class Ejercicio_13 {
 
         }
         System.out.println("Introduce un numero para insentar");
-        int numero= entrada.nextInt();
+        int numero=entrada.nextInt();
 
-        int i = 5;
+        int i = 3;
         boolean aux=false;
         int posicion = 0;
+        int auxBucle=0;
 
 
-        for (int j = 0; j < array.length-1; j++) {
-            if (numero>array[i]){
-                posicion = i;
+        do {
+            posicion= auxBucle;
 
-            }
+            auxBucle+=1;
 
-
-        }
-
+        }while(numero>array[auxBucle]);
+        int auxInsertar = 0;
         for (int j = array.length-2; j >=posicion ; j--) {
             array[j+1]=array[j];
+            auxInsertar =j;
+
+
         }
+        array[auxInsertar+1]=numero;
+
 
        for (int j = 0; j < array.length; j++) {
             if (j != array.length - 1) {
@@ -51,15 +55,7 @@ public class Ejercicio_13 {
 
 
 
-        for (int j = 0; j < array.length; j++) {
-            if (j != array.length - 1) {
-                System.out.print(array[j] + ",");
-            } else {
-                System.out.println(array[j]);
-            }
 
-
-        }
 
 
 
