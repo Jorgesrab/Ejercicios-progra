a447f2f0b492958ecb6d04e4b8a7e9cc8f5235d9
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2024-04-18 13:32:22 +0200

    cambios

---
+++ src/Parte_2/Entornos/Ejercicios_pooAvanzado/Ejercicio2_Producto/Main.java
@@ -0,0 +1,32 @@
+package Parte_2.Entornos.Ejercicios_pooAvanzado.Ejercicio2_Producto;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        Producto producto1 = new Producto("Platano",6);
+        Producto producto2 = new Producto("Melon",3);
+        Producto producto3 = new Producto("Leche",7);
+        Producto producto4 = new Producto("Carne",12);
+        Producto producto5 = new Producto("pescado",2);
+
+        ArrayList<Producto> Productos = new ArrayList<Producto>();
+        Productos.add(producto1);
+        Productos.add(producto2);
+        Productos.add(producto3);
+        Productos.add(producto4);
+        Productos.add(producto5);
+
+        Iterator<Producto> iterador = Productos.iterator();
+        while (iterador.hasNext()){
+            Producto producto =iterador.next();
+            System.out.println(producto.nombre);
+        }
+
+        Productos.remove(2);
+
+
+    }
+}
---
+++ src/Parte_2/Entornos/Ejercicios_pooAvanzado/Ejercicio2_Producto/Producto.java
@@ -0,0 +1,29 @@
+package Parte_2.Entornos.Ejercicios_pooAvanzado.Ejercicio2_Producto;
+
+public class Producto {
+    String nombre;
+    int cantidad;
+
+    public Producto(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+
+}
